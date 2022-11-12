1369a32bb97cf1bd7bbfdee9b487b8a57f533ae3
Danielle <dmbecker24@gmail.com>
2022-11-11 21:08:28 -0800

    Configure survey views

--- survey/views.py
+++ survey/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Survey
+from .forms import SurveyForm
 
 def index(request):
-    return HttpResponse("Hello world")
+    return render(request, "survey/survey.html")
+
+def show_survey(request, id=None):
+    survey = get_object_or_404(Survey, pk=id)
+    form = SurveyForm(survey)
+    
+    context = {
+      "survey": survey,
+      "form": form,
+    }
+    return render(request, "survey/survey.html", context)
